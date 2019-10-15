6669dea2972dc9b3c8bb627ff1a26f7ea85dea43
mshanemc <shane.mclaughlin@salesforce.com>
2019-10-15 12:20:09 -0500

    images plus produts as json

--- src/modules/my/app/app.ts
+++ src/modules/my/app/app.ts
@@ -1,3 +1,27 @@
 import { LightningElement } from 'lwc';
 
-export default class App extends LightningElement {}
+export default class App extends LightningElement {
+    products = [
+        {
+            imgSrc: 'https://via.placeholder.com/150',
+            title: 'Roof mounted stowable solar array',
+            description:
+                'Charge your car anywhere. Mounts securely to roof rack, plugs into charging port, and folds flat to stow in cargo compartment. Requires roof rack. Great for camping, parking lots. Use only while vehicle is parked.',
+            price: 695
+        },
+        {
+            imgSrc: '/resources/assets/images/spoiler.jpg',
+            title: 'Sport Spoiler',
+            description:
+                'This spoiler mounts to the rear of your car, improving looks and aerodynamics. Please see the product guide for speed-force curves and details on performance and efficiency impacts.',
+            price: 295
+        },
+        {
+            imgSrc: '/resources/assets/images/dalmationharness.jpg',
+            title: 'Pet-Friendly Travel Kit',
+            description:
+                'Hit the road with your pets and make sure they ride in safety and style. Kit includes a large dog harness, secure fastens for kennels and a pet blanket to protect your seats.',
+            price: 95
+        }
+    ];
+}
